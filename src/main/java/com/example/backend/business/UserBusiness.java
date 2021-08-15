93eb6184d6a08254a5cfbae2c999d6ae3b99d08a
CsignS <smileroom2010@gmail.com>
2021-08-15 16:50:47 +0700

    EP2 Uploadfile Database postgs sql test tag

--- src/main/java/com/example/backend/business/UserBusiness.java
+++ src/main/java/com/example/backend/business/UserBusiness.java
@@ -28,6 +28,8 @@ public class UserBusiness {
         //TODO:mapper
         return user;
 
+        //test tag
+
         /* Ep1
     public String register(MRegisterRequest request) throws BaseException {
 
